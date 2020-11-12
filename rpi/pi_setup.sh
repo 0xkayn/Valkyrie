caed0e640a2e1663dc0f46122f9d7e92a8399f0e
vuggo <vuggoA@gmail.com>
2020-11-12 07:26:51 -0500

    mavsh_init working

--- rpi/pi_setup.sh
+++ rpi/pi_setup.sh
@@ -1,4 +1,7 @@
 sudo apt update && sudo apt -y upgrade
 sudo apt install python3-pip
+sudo apt- install python-pip python-dev
+pip3 install dronekit
 
-pip3 install MAVProxy dronekit
+git clone my_MAVProxy && cd my_MAVProxy
+python3 setup.py build
