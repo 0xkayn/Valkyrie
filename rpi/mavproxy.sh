5ee99a41edbd5dc799b72ed6249cfec5619f4ece
vuggo <vuggoA@gmail.com>
2020-11-13 09:59:57 -0500

    updated ardupilot to build properly

---
+++ rpi/mavproxy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mavproxy.py --mavversion=2.0 --source-system=1 --source-component=254 --target-system=
--- rpi/pi_setup.sh
+++ rpi/pi_setup.sh
@@ -1,7 +1,26 @@
 sudo apt update && sudo apt -y upgrade
-sudo apt install python3-pip
-sudo apt- install python-pip python-dev
+sudo apt remove -y python2 python3 
+
+sudo apt-get install -y build-essential tk-dev libncurses5-dev \
+libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev \
+libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+
+version=3.8.6
+
+wget https://www.python.org/ftp/python/$version/Python-$version.tgz
+tar zxf Python-$version.tgz
+cd Python-$version
+./configure --enable-optimizations
+make -j4
+sudo make altinstall
+# holy fuck this takes so long to do on a pi0w...
+sudo apt install -y python3-pip 
 pip3 install dronekit
 
-git clone my_MAVProxy && cd my_MAVProxy
-python3 setup.py build
+git clone https://github.com/Vuggo/Valkyrie
+VPATH=$PWD/Valkyrie
+cd Valkyrie/ardupilot/modules/mavlink/pymavlink
+python3 setup.py install --user
+cd $VPATH/MAVProxy
+python3 setup.py build install --user
+
